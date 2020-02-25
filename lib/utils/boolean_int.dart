e3f34e12f279df8172956e6a3333dc65c4cb852f
Guillaume Roux <guillaume2.roux@epitech.eu>
2020-02-25 17:44:25 +0100

    rework function int to bool

--- lib/utils/boolean_int.dart
+++ lib/utils/boolean_int.dart
@@ -1,12 +1,3 @@
 int booleanToInt(bool val) => val ? 1 : 0;
 
-bool intToBool(int val) {
-  switch (val) {
-    case 1:
-      return true;
-    case 0:
-      return false;
-    default:
-      return null;
-  }
-}
+bool intToBool(int val) => val == 1 ? true : val == 0 ? false : null;
