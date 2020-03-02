8d6a30912ac4d46af51126e3fd3864e178da86da
Guillaume Roux <guillaume2.roux@epitech.eu>
2020-03-02 16:14:59 +0100

    update version number

---
+++ lib/TemtemPage/widgets/CatchRateCard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CatchRateCard extends StatelessWidget {
+  final int catchRate;
+
+  CatchRateCard(this.catchRate);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/TemtemPage/widgets/GenderRatioCard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:temopedia/Models/GenderRatio.dart';
+
+class GenderRatioCard extends StatelessWidget {
+  final GenderRatio genderRatio;
+
+  GenderRatioCard(this.genderRatio);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
