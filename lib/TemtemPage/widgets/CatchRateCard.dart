2eafca9954c7f1308728499d52bc2c1d338a9924
Guillaume Roux <guillaume2.roux@epitech.eu>
2020-03-02 16:38:47 +0100

    added catchrate

--- lib/TemtemPage/widgets/CatchRateCard.dart
+++ lib/TemtemPage/widgets/CatchRateCard.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:temopedia/styles/Theme.dart';
 
 class CatchRateCard extends StatelessWidget {
   final int catchRate;
+
+  final textStyle = TextStyle(height: 0.8, color: MyColors.lightFont);
 
   CatchRateCard(this.catchRate);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+      decoration: BoxDecoration(
+          color: MyColors.background, borderRadius: BorderRadius.circular(21)),
+      child: Text("Catch rate: $catchRate", style: textStyle),
+    );
   }
 }
