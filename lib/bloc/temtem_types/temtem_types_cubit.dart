3bf53118a45573f374ac0a3466a1d2cd3699c4a6
Guillaume Roux <rouxguillaume8@gmail.com>
2022-09-14 17:45:01 +0200

    added fetch type usecase

--- lib/bloc/temtem_types/temtem_types_cubit.dart
+++ lib/bloc/temtem_types/temtem_types_cubit.dart
@@ -2,8 +2,32 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:temtem_api_wrapper/temtem_api_wrapper.dart';
 
+import '../../domain/use_cases/fetch_tem_types.dart';
+
 part 'temtem_types_state.dart';
 
 class TemtemTypesCubit extends Cubit<TemtemTypesState> {
-  TemtemTypesCubit() : super(const TemtemTypesLoading());
+  TemtemTypesCubit({
+    required FetchTemTypesUseCase fetchTemTypesUseCase,
+  })  : _fetchTemTypesUseCase = fetchTemTypesUseCase,
+        super(const TemtemTypesLoading()) {
+    fetchTemtemTypes();
+  }
+
+  final FetchTemTypesUseCase _fetchTemTypesUseCase;
+
+  List<TemTemApiType> _temtemTypes = [];
+  List<TemTemApiType> get temtemTypes => _temtemTypes;
+
+  Future<void> fetchTemtemTypes() async {
+    emit(const TemtemTypesLoading());
+    final result = await _fetchTemTypesUseCase.call();
+    result.fold(
+      (success) {
+        _temtemTypes = success;
+        emit(TemtemTypesLoaded(temtemTypes: success));
+      },
+      (error) => emit(TemtemTypesError(message: error.toString())),
+    );
+  }
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,8 +4,11 @@ import 'package:temtem_api_wrapper/temtem_api_wrapper.dart';
 
 import 'core/network_info.dart';
 import 'data/datasources/remote/remote_datasource.dart';
+import 'data/repositories/temtem_types_repository.dart';
 import 'data/repositories/temtems_repository.dart';
+import 'domain/repository/temtem_type_repository.dart';
 import 'domain/repository/temtems_repository.dart';
+import 'domain/use_cases/fetch_tem_types.dart';
 import 'domain/use_cases/fetch_temtems.dart';
 
 final sl = GetIt.instance;
@@ -23,9 +26,16 @@ Future<void> setupLocator() async {
       networkInfo: sl(),
     ),
   );
+  sl.registerLazySingleton<TemtemTypesRepository>(
+    () => TemtemTypesRepositoryImpl(
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
 
   // Use cases
   sl.registerLazySingleton(() => FetchTemtemsUseCase(sl()));
+  sl.registerLazySingleton(() => FetchTemTypesUseCase(sl()));
 
   // External
   sl.registerLazySingleton<NetworkInfo>(
